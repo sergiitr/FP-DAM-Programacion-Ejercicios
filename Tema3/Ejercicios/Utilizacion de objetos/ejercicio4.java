6e63134411e9455d38e0f78e559541b21ec8baf6
Sergio Trillo Rodriguez <86511887+sergiitr@users.noreply.github.com>
2024-10-24 12:30:43 +0200

    Update ejercicio4.java

--- Tema3/Ejercicios/Utilizacion de objetos/ejercicio4.java
+++ Tema3/Ejercicios/Utilizacion de objetos/ejercicio4.java
@@ -1,23 +1,35 @@
 public class ejercicio4 {
     public static void main(String[] args) {
         /**
-         * Se muestra el total de argumentos introducidos
-         */
-        System.out.println("Se han introducido un total de "+args.length+" argumentos");
-        /**
-         * Se lanza una excepcion si no se introducen datos o son menores que 3
-         * Se muestra por pantalla si se introducen 3 o mas
+         * Se lanza una excepción si no se introducen datos o son menores que 3.
+         * Si se introducen 3 o más, se muestra un mensaje de bienvenida.
          */
         try {
-            if(args.length==3)
-                System.out.println("Bienvenido "+args[2]+" "+args[0]+" "+args[1]);
-            else if (args.length >=3) {
-                System.out.println("Se han metido mas parmetros, se generara con los 3 primeros");
-                System.out.println("Bienvenido "+args[2]+" "+args[0]+" "+args[1]);
+            /**
+             * Si el número de argumentos es exactamente 3
+             * Mostrar mensaje de bienvenida usando los tres argumentos (nombre completo)
+             */
+            if(args.length == 3)
+                System.out.println("Bienvenido " + args[2] + " " + args[0] + " " + args[1]);
+            /**
+             * Si hay más de 3 argumentos
+             * Notificar que se han introducido más parámetros de los necesarios
+             * Mostrar mensaje de bienvenida usando solo los tres primeros argumentos
+             */
+            else if (args.length > 3) {
+                System.out.println("Se han metido más parámetros, se generará con los 3 primeros");
+                System.out.println("Bienvenido " + args[2] + " " + args[0] + " " + args[1]);
+            } else {
+                /**
+                 * Lanzar una excepción si se proporcionan menos de 3 argumentos
+                 */
+                throw new Exception();
             }
         } catch (Exception e) {
-            System.out.println("Datos insuficientes");
+            /**
+             * Captura la excepción (que no haya suficientes argumentos)
+             */
+            System.out.println("Datos insuficientes. Se debe proporcionar al menos 3 parámetros.");
         }
-        
     }
 }

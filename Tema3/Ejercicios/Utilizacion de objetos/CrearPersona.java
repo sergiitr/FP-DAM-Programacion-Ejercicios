c739e240a8198b4fe0de691c2e1705b741e1ec1c
Sergio Trillo Rodriguez <86511887+sergiitr@users.noreply.github.com>
2024-10-24 19:14:00 +0200

    Update CrearPersona.java

--- Tema3/Ejercicios/Utilizacion de objetos/CrearPersona.java
+++ Tema3/Ejercicios/Utilizacion de objetos/CrearPersona.java
@@ -20,6 +20,11 @@ class Persona {
         this.fNac=fnac;
     }
     
+    /**
+     * Comprobamos si ambos objetos son iguales o no con ==
+     * @param p2 de tipo Persona
+     * @return true si son iguales false si son distintos
+     */
     public boolean comparador(Persona p2) {
         if(this.nombre == p2.nombre && this.apellidos == p2.apellidos && this.fNac == p2.fNac)
             return true;
@@ -27,6 +32,11 @@ class Persona {
             return false;
     }
 
+    /**
+     * Comprobamos si ambos objetos son iguales o no con el metodo equals
+     * @param p2 de tipo Persona
+     * @return true si son iguales false si son distintos
+     */
     public boolean comparador2(Persona p2) {
         if(this.nombre.equals(p2.nombre) && this.apellidos.equals(p2.apellidos) && this.fNac.equals(p2.fNac))
             return true;
@@ -81,18 +91,18 @@ public class CrearPersona{
         System.out.println(p1.getfNac().format(formatoFecha).toString());
 
         System.out.println("\n--- Datos Persona 2 ---");
-        LocalDate fechaPrueba=LocalDate.of(2024, 12, 12);
-        Persona p2 = new Persona("Prueba2","Apellidos2",fechaPrueba);
+        LocalDate fechaPrueba2=LocalDate.of(2001, 07, 02);
+        Persona p2 = new Persona("Sergio","Trillo",fechaPrueba2);
         System.out.println(p2.getNombre());
         System.out.println(p2.getApellidos());
         System.out.println(p2.getfNac().format(formatoFecha).toString());
-
         System.out.println("\n--- Datos Persona 3 ---");
-        LocalDate fechaPrueba2=LocalDate.of(2001, 07, 02);
-        Persona p3 = new Persona("Sergio","Trillo",fechaPrueba2);
+        LocalDate fechaPrueba=LocalDate.of(2024, 12, 12);
+        Persona p3 = new Persona("Prueba2","Apellidos2",fechaPrueba);
         System.out.println(p3.getNombre());
         System.out.println(p3.getApellidos());
         System.out.println(p3.getfNac().format(formatoFecha).toString());
+
 
         System.out.println("\n --- Resultados ---");
         System.out.println("-- Comparador ");
@@ -103,7 +113,42 @@ public class CrearPersona{
             System.out.println("Los objetos son distintos");
 
         System.out.println("Comparamos p1 y p3");
+        if (p1.comparador(p3) == true)
+            System.out.println("Los objetos son iguales");
+        else
+            System.out.println("Los objetos son distintos");
+
+
+        System.out.println("\n-- Comparador2 ");
+        System.out.println("Comparamos p1 y p2");
+        if (p1.comparador2(p2) == true)
+            System.out.println("Los objetos son iguales");
+        else
+            System.out.println("Los objetos son distintos");
+
+        System.out.println("Comparamos p1 y p3");
         if (p1.comparador2(p3) == true)
+            System.out.println("Los objetos son iguales");
+        else
+            System.out.println("Los objetos son distintos");
+
+        // Ponemos que la persona 1 tenga los atributos que la persona 2
+        System.out.println("\n\nAsociamos p2 a p1");
+        p1 = p2;
+        System.out.println("Mostramos los nuevos datos de p1");
+        System.out.println(p2.getNombre());
+        System.out.println(p2.getApellidos());
+        System.out.println(p2.getfNac().format(formatoFecha).toString());
+
+        System.out.println("\n-- Comparador ");
+        System.out.println("Comparamos p1 y p2");
+        if (p1.comparador(p2) == true)
+            System.out.println("Los objetos son iguales");
+        else
+            System.out.println("Los objetos son distintos");
+
+        System.out.println("Comparamos p1 y p3");
+        if (p1.comparador(p3) == true)
             System.out.println("Los objetos son iguales");
         else
             System.out.println("Los objetos son distintos");

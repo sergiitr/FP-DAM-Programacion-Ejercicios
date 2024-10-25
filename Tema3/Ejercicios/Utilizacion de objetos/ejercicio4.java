75e744f9a07862954a57c4909ba850341ab42667
Sergio Trillo Rodriguez <86511887+sergiitr@users.noreply.github.com>
2024-10-25 18:11:21 +0200

    Update ejercicio4.java

--- Tema3/Ejercicios/Utilizacion de objetos/ejercicio4.java
+++ Tema3/Ejercicios/Utilizacion de objetos/ejercicio4.java
@@ -1,5 +1,12 @@
+import java.time.LocalDate;
+
 public class ejercicio4 {
     public static void main(String[] args) {
+        String nombre = args[2];
+        String apellidos = args[0].concat(" "+args[1]);
+        
+
+        Persona p1=new Persona(nombre, apellidos);
         /**
          * Se lanza una excepción si no se introducen datos o son menores que 3.
          * Si se introducen 3 o más, se muestra un mensaje de bienvenida.
@@ -10,7 +17,7 @@ public class ejercicio4 {
              * Mostrar mensaje de bienvenida usando los tres argumentos (nombre completo)
              */
             if(args.length == 3)
-                System.out.println("Bienvenido " + args[2] + " " + args[0] + " " + args[1]);
+                System.out.println("Bienvenido "+ p1.getNombre() + " " + p1.getApellidos());
             /**
              * Si hay más de 3 argumentos
              * Notificar que se han introducido más parámetros de los necesarios
@@ -18,13 +25,12 @@ public class ejercicio4 {
              */
             else if (args.length > 3) {
                 System.out.println("Se han metido más parámetros, se generará con los 3 primeros");
-                System.out.println("Bienvenido " + args[2] + " " + args[0] + " " + args[1]);
-            } else {
+                System.out.println("Bienvenido "+ p1.getNombre() + " " + p1.getApellidos());
+            } else 
                 /**
                  * Lanzar una excepción si se proporcionan menos de 3 argumentos
                  */
                 throw new Exception();
-            }
         } catch (Exception e) {
             /**
              * Captura la excepción (que no haya suficientes argumentos)

6508a8b8a2b476e01d7dfac047b83c349ba9c321
Sergio Trillo Rodriguez <86511887+sergiitr@users.noreply.github.com>
2024-10-31 18:50:35 +0100

    Update ejercicio3.java

--- Tema3/Ejercicios/Uso de metodos/ejercicio3.java
+++ Tema3/Ejercicios/Uso de metodos/ejercicio3.java
@@ -1,7 +1,15 @@
 public class ejercicio3 {
     public static void main(String[] args) {
-        RegEx c1 = new RegEx(26518076);
-        System.out.println("Mi DNI es: "+c1.getDNI());
-
+        EcuacionSegundoGrado c1 = new EcuacionSegundoGrado(1,1,3);
+        try {
+            System.out.println("La solucion positiva es: "+c1.solucion1());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        try {
+            System.out.println("La solucion positiva es: "+c1.solucion2());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }

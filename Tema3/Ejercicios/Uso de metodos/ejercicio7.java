651b94f8c6575532795850d41de736e28b411869
Sergio Trillo Rodriguez <86511887+sergiitr@users.noreply.github.com>
2024-11-04 17:27:30 +0100

    Update ejercicio7.java

--- Tema3/Ejercicios/Uso de metodos/ejercicio7.java
+++ Tema3/Ejercicios/Uso de metodos/ejercicio7.java
@@ -2,14 +2,22 @@ import java.util.Scanner;
 public class ejercicio7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        /**
+         * Se crea la primera distancia con el constructor x defecto
+         */
         Distancia2Puntos d1 = new Distancia2Puntos();
         System.out.println("La distancia es: "+d1.distancia()+"cm");
 
+        /**
+         * Se crea la segunda distancia con el constructor de copia
+         */
         Distancia2Puntos d2 = new Distancia2Puntos(5,2,6,4);
         System.out.println("La distancia es: "+d2.distancia()+"cm");
 
-
-        /**Distancia2Puntos d3 = new Distancia2Puntos();
+        /**
+         * Se crea la tercera distancia pasandole los parametros x consola
+         */
+        Distancia2Puntos d3 = new Distancia2Puntos();
         int a1,b1, a2,b2;
         System.out.println("Introduzca coordenadas (x,y) del pnto A:");
         a1=sc.nextInt();
@@ -23,6 +31,6 @@ public class ejercicio7 {
         d3.setX2(a2);
         d3.setY1(b1);
         d3.setY2(b2);
-        System.out.println("La distancia es: "+d3.distancia()+"cm"); */
+        System.out.println("La distancia es: "+d3.distancia()+"cm"); 
     }
 }

9dfe1159fc3ab787211a360d59d6af4ff458f245
Sergio Trillo Rodriguez <86511887+sergiitr@users.noreply.github.com>
2024-11-08 17:59:59 +0100

    Update ejercicio1.java

--- Tema3/Entregas/ejercicio1.java
+++ Tema3/Entregas/ejercicio1.java
@@ -5,5 +5,17 @@ public class ejercicio1 {
 
         EcuacionesLineales e2 = new EcuacionesLineales(2,1,-1,2,7,-1);
         System.out.println("Ecuacion 2:\n"+e2.toString());
+
+        EcuacionesLineales e3 = new EcuacionesLineales(1,1,2,2,1,2);
+
+        if (e1.equals(e2) == true)
+            System.out.println("e1 y e2 son iguales");
+        else
+            System.out.println("e1 y e2 NO son iguales");
+
+        if (e1.equals(e3) == true)
+            System.out.println("e1 y e3 son iguales");
+        else
+            System.out.println("e1 y e3 NO son iguales");
     }
 }

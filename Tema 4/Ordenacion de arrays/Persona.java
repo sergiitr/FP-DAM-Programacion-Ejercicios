c966fbfaac458bc4bc072bd196cca0635f6f3bec
Sergio Trillo Rodriguez <86511887+sergiitr@users.noreply.github.com>
2024-11-22 17:38:55 +0100

    Add files via upload

---
+++ Tema 4/Ordenacion de arrays/Persona.java
@@ -0,0 +1,49 @@
+class Persona {
+    /**
+     * Constructor por defecto
+     */
+    public Persona(){
+        this.nombre="Prueba2";
+        this.edad=12;
+    }
+
+    /**
+     * Constructor de copia para el ejercicio 3
+     * @param nombre
+     * @param apellidos
+     * @param fnac
+     */
+    public Persona(String nombre, int edad){
+        this.nombre=nombre;
+        this.edad=edad;
+    }
+
+    
+
+    /**
+     * Ponemos los setters de los parametros
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    /**
+     * Ponemos los getters de los parametros
+     */
+    public String getNombre(){
+        return nombre;
+    }    
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * Ponemos los atributos 
+     */
+    private String nombre;
+    private int edad;
+    
+}

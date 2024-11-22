869fb9d668e3c438e7b1322e3e19ab900e883566
Sergio Trillo Rodriguez <86511887+sergiitr@users.noreply.github.com>
2024-11-22 17:50:08 +0100

    Add files via upload

---
+++ Tema 4/Ordenacion de arrays/Estudiante.java
@@ -0,0 +1,45 @@
+public class Estudiante {
+    /**
+     * Constructor por defecto
+     */
+    public Estudiante(){
+        this.nombre="Prueba2";
+        this.nota=6;
+    }
+
+    /**
+     * Constructor de copia para el ejercicio 3
+     * @param nombre
+     * @param apellidos
+     * @param fnac
+     */
+    public Estudiante(String nombre, int nota){
+        this.nombre=nombre;
+        this.nota=nota;
+    }
+    /**
+     * Ponemos los setters de los parametros
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    /**
+     * Ponemos los getters de los parametros
+     */
+    public String getNombre(){
+        return nombre;
+    }    
+    public int getNota() {
+        return nota;
+    }
+
+    /**
+     * Ponemos los atributos 
+     */
+    private String nombre;
+    private int nota;
+}

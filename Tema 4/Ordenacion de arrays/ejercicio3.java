0cba63301845e33f2c16e4d3e76ceede725dc953
Sergio Trillo Rodriguez <86511887+sergiitr@users.noreply.github.com>
2024-11-25 16:02:16 +0100

    Update ejercicio3.java

--- Tema 4/Ordenacion de arrays/ejercicio3.java
+++ Tema 4/Ordenacion de arrays/ejercicio3.java
@@ -2,55 +2,80 @@ import java.util.Scanner;
 
 public class ejercicio3 {
     //Metodo de Ordenamiento QuickSort
-    public static void Quicksort(int numeros[], int izq, int der){
+    public static void Quicksort(Estudiante personas[], int izq, int der){
         /**
          * Este metodo recibe un arreglo de numero, y dos enteros que referencian el primer valor y el ultimo
          */
         //Se toma como pivote el primer valor
-        int pivote = numeros[izq];
+        Estudiante pivote = personas[izq];
         
         //Se definen los dos lados y un auxiliar
         int i = izq; 
         int j = der; 
-        int aux;
+        Estudiante aux;
         
         while(i<j) {
-            while (numeros[i] <= pivote && i < j) 
+            while (personas[i].getNota() <= pivote.getNota() && i < j) 
                 i++;
-            while (numeros[j] > pivote) 
+            while (personas[j].getNota() > pivote.getNota()) 
                 j--;   
 
             if (i<j) {                                     
-                aux = numeros[i];                  
-                numeros[i]= numeros[j];
-                numeros[j]=aux;
+                aux = personas[i];                  
+                personas[i]= personas[j];
+                personas[j]=aux;
             }
         }
 
-        numeros[izq] = numeros[j]; 
-        numeros[j] = pivote;
+        personas[izq] = personas[j]; 
+        personas[j] = pivote;
 
         if (izq < j-1)
-            Quicksort(numeros,izq,j-1);
+            Quicksort(personas,izq,j-1);
 
         if (j+1 < der)
-            Quicksort(numeros,j+1,der);
+            Quicksort(personas,j+1,der);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] vector = new int[10];
-        final int N = vector.length;
+        Estudiante[] estudiantes = new Estudiante[10];
+        final int N = estudiantes.length;
+        for (int i=0;i<N;i++)
+            estudiantes[i] = new Estudiante();
         
-        System.out.println("Introduce 10 notas:");
+        String[] nombre = new String[10];
+        System.out.println("--- Introduce los nombre ---");
         for (int i=0; i<N; i++) {
-            System.out.print("Nota "+(i+1)+":");
-            vector[i] = sc.nextInt();
+            System.out.print("Persona "+(i+1)+": ");
+            nombre[i]=sc.nextLine();
         }
-        Quicksort(vector,0,(vector.length-1));
-        System.out.print("Nuestro vector es: [");
-        for (int i=0; i<N; i++)
-            System.out.print(vector[i]+" ");
-        System.out.println("]");
+        for (int i=0; i<N; i++) 
+            estudiantes[i].setNombre(nombre[i]);
+        
+
+        System.out.println("--- Introduce las edades ---");
+        int nota;
+        for (int i=0; i<N; i++) {
+            System.out.print("Persona "+(i+1)+": ");
+            nota=sc.nextInt();
+            estudiantes[i].setNota(nota);
+        }
+
+        System.out.println("Vector antes:");
+        for (Estudiante i:estudiantes) {
+            System.out.println("Nombre: "+i.getNombre());
+            System.out.println("Edad: "+i.getNota());
+            System.out.println();
+        }
+
+        Quicksort(estudiantes,0,(estudiantes.length-1));
+        
+        System.out.println("Vector despues:");
+        for (Estudiante i:estudiantes) {
+            System.out.println("Nombre: "+i.getNombre());
+            System.out.println("Edad: "+i.getNota());
+            System.out.println();
+        }
     }    
 }

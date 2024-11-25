1ea7df35875ad7feabb198ed0623909f46107ede
Sergio Trillo Rodriguez <86511887+sergiitr@users.noreply.github.com>
2024-11-25 17:31:37 +0100

    Delete Tema3 directory

--- Tema3/Ejercicios/Uso de metodos/ejercicio1.java
+++
@@ -1,9 +0,0 @@
-public class ejercicio1 {
-    public static void main(String[] args) {
-        Cilindro c1 = new Cilindro(3,3);
-        System.out.println(c1.toString());
-
-        Cilindro c2 = new Cilindro();
-        System.out.println(c2.toString());
-    }
-}
--- Tema3/Ejercicios/Uso de metodos/ejercicio2.java
+++
@@ -1,6 +0,0 @@
-public class ejercicio2 {
-    public static void main(String[] args) {
-        TrianguloRectangulo tr1 = new TrianguloRectangulo(4,3);
-        System.out.println(tr1.toString());
-    }
-}

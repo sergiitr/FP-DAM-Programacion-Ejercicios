4255b0b0fe41ed155b472d27e3598ea5ed144249
Sergio Trillo Rodriguez <86511887+sergiitr@users.noreply.github.com>
2024-12-04 17:32:33 +0100

    Update Cuenta.java

--- Tema 3/Repaso/Clases/Cuenta.java
+++ Tema 3/Repaso/Clases/Cuenta.java
@@ -1,30 +1,30 @@
 class Cuenta {
     public Cuenta() {
     }
+    
+    public Cuenta(String titular) {
+        this.titular = titular;
+        this.cantidad=0;
+    }
 
-    public Cuenta(String titular, int cantidad) {
+    public Cuenta(String titular, double cantidad) {
         this.titular = titular;
         this.cantidad = cantidad;
     }
-
 
     public String getTitular() {
         return titular;
     }
 
-    public void setTitular(String titular) {
-        this.titular = titular;
-    }
 
-    public int getCantidad() {
+    public double getCantidad() {
         return cantidad;
     }
 
-    public void setCantidad(int cantidad) {
+    public void setCantidad(double cantidad) {
         this.cantidad = cantidad;
     }
 
-
     private String titular;
-    private int cantidad;
+    private double cantidad;
 }

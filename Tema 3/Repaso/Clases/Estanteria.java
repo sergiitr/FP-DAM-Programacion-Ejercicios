f5ec0d49f9e2c9b536f311b1f4a7da6ae3b587b1
Sergio Trillo Rodriguez <86511887+sergiitr@users.noreply.github.com>
2024-12-05 19:43:44 +0100

    Update Estanteria.java

--- Tema 3/Repaso/Clases/Estanteria.java
+++ Tema 3/Repaso/Clases/Estanteria.java
@@ -1,17 +1,41 @@
 public class Estanteria {
-    public void guardarLibro(Libro libro) {
+    public Estanteria(){
+    }
+    public Estanteria(int librosGuardados) {
+        this.librosGuardados = librosGuardados;
+    }
+    public Libros[] getLibros() {
+        return libros;
+    }
+
+    public void setLibros(Libros[] libros) {
+        this.libros = libros;
+    }
+    public String guardarLibro(Libro libro) {
         if(librosGuardados<CANT_LIBROS) {
             libros[librosGuardados]=libro;
         }
+        return "Libro "+libro.titulo()+" guardado";
     }
-    public void sacarLibro(Libro libro) {
-        if(librosGuardados<CANT_LIBROS) {
-            libros[librosGuardados]=null;
-            librosGuardados--;
+    public boolean sacarLibro(Libro libro) {
+        int auxiliar;
+        
+        for (int i=0; i<librosGuardados;i++) {
+            if (libro.equals(this.libros[i])){
+                this.libros[i]=null;
+                auxiliar=i;
+            }
         }
+        for (int i=auxiliar; i<librosGuardados;i++) {
+            this.libros[i] =this.libros[i+1];
+        }
+        librosGuardados-=1;
+
     }
 
     private final int CANT_LIBROS=10;
-    private Libros[CANT_LIBROS] libros;
+    private Libros[] libros=new Libros[CANT_LIBROS];
     private int librosGuardados;
+
+    
 }

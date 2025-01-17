dd1c73aee2568c9c08ca2de5e7c199adec6bb49c
Sergio Trillo Rodriguez <86511887+sergiitr@users.noreply.github.com>
2025-01-17 12:43:07 +0100

    Update ejercicio1.java

--- Tema 4/Cadena de caracteres/ejercicio1.java
+++ Tema 4/Cadena de caracteres/ejercicio1.java
@@ -6,25 +6,16 @@ public class ejercicio1 {
         System.out.print("Introduzca la frase: ");
         String frase = sc.nextLine();
 
-        // Convertimos la frase a minúsculas para ignorar mayúsculas/minúsculas
         frase = frase.toLowerCase();
-
-        // Creamos un array de 26 posiciones para contar cada letra (a-z)
         int[] contador = new int[26];
 
-        // Recorremos la frase y contamos solo las letras
         for (int i = 0; i < frase.length(); i++) {
             char caracter = frase.charAt(i);
-            if (caracter >= 'a' && caracter <= 'z') {
-                contador[caracter - 'a']++; // Incrementa el índice correspondiente a la letra
-            }
+            if (caracter >= 'a' && caracter <= 'z') 
+                contador[caracter - 'a']++;  
         }
-
-        // Imprimimos los resultados
-        for (int i = 0; i < contador.length; i++) {
-            if (contador[i] > 0) { // Solo mostramos letras que aparecen en la frase
+        for (int i = 0; i < contador.length; i++) 
+            if (contador[i] > 0) 
                 System.out.println((char) (i + 'a') + ": " + contador[i]);
-            }
-        }
     }
 }

0b9d804f8a95381baf7b3ac6ae437b44c582767c
Sergio Trillo Rodriguez <86511887+sergiitr@users.noreply.github.com>
2025-01-29 16:08:50 +0100

    Update Banco.java

--- Tema 3.5/Practica/Banco.java
+++ Tema 3.5/Practica/Banco.java
@@ -6,17 +6,19 @@ public class Banco {
     }
 
     public CuentaBancaria buscaCuenta(int numero) {
+        for (int i=0; i<this.contador; i++) 
+            if ( this.cuentas[i].getNumeroCuenta() == numero )
+                return this.cuentas[i];
 
+        return null;
     } 
 
     public void listarCuentas() {
-        if (this.contador==0) {
+        if (this.contador==0)
             System.out.println("No hay cuentas disponibles");
-        } else {
-            for (int i=0; i<this.contador; i++) {
+        else
+            for (int i=0; i<this.contador; i++) 
                 System.out.println(this.cuentas[i].toString());
-            }
-        }
     }
 
     private CuentaBancaria[] cuentas = new CuentaBancaria[100];

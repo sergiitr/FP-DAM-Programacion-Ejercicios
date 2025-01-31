1060accf1b9c42d2e4484abd60c5e833637bc9d0
Sergio Trillo Rodriguez <86511887+sergiitr@users.noreply.github.com>
2025-01-31 17:49:38 +0100

    Update ejercicio2.java

--- Tema 5/Ejercicios/Herencia/ejercicio2/ejercicio2.java
+++ Tema 5/Ejercicios/Herencia/ejercicio2/ejercicio2.java
@@ -1,5 +1,11 @@
 public class ejercicio2 {
     public static void main(String[] args) {
-        
+        Vehiculos v1 = new Vehiculos("1234ABC", "Nissan", 1000);
+        Taxi t1 = new Taxi("1111AAA", "ASD", 1500, 2000);
+        Autobus a1 = new Autobus(30, "2222BBB", "ZXC", 1200);
+
+        System.out.println("Vehiculos: "+v1.toString());
+        System.out.println("Taxi:"+t1.toString());
+        System.out.println("Autobus:"+a1.toString());
     }
 }

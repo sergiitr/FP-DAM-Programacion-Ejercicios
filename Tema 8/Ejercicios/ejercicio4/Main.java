869af042ebe1325bda0a5d11198f913c2f2bee06
Sergio Trillo Rodriguez <86511887+sergiitr@users.noreply.github.com>
2025-04-25 16:51:19 +0200

    Update Main.java

--- Tema 8/Ejercicios/ejercicio4/Main.java
+++ Tema 8/Ejercicios/ejercicio4/Main.java
@@ -17,7 +17,7 @@ public class Main {
             System.out.println("---- OPCIONES ----");
             System.out.println("1) Añadir un nuevo alumno. Deberás pedir los datos del alumno y añadirlo a la tabla correspondiente");
             System.out.println("2) Matricular a un alumno en una asignatura. Deberás pedir el DNI del alumno y matricularlo en todas las asignaturas cuyo nombre esté contenido en un texto solicitado al usuario.");
-            System.out.println("3) Actualiza el email de todos los alumnos al dominio que se solicite (por ejemplo, @g.educaand.es).");
+            System.out.println("3) Actualiza el email de todos los alumno al dominio que se solicite (por ejemplo, @g.educaand.es).");
             System.out.println("4) Eliminar a un alumno por su DNI, incluyendo su eliminación de la tabla “matricula”.");
 
             int opcion;
@@ -66,15 +66,24 @@ public class Main {
                         String correo;
                         System.out.print("Introduce dominio que deseas actualizar (ej: @gmail.com): ");
                         correo=sc.nextLine();
-                        String sqlInsert3 = "UPDATE alumnos SET email = CONCAT(LEFT(email, LOCATE('@', email) - 1), '"+correo+"');";
+                        String sqlInsert3 = "UPDATE alumno SET email = CONCAT(LEFT(email, LOCATE('@', email) - 1), '"+correo+"');";
                         Statement stmt3 = conexion.createStatement();
-                        int rowsAffected3 = stmt2.executeUpdate(sqlInsert3);
+                        int rowsAffected3 = stmt3.executeUpdate(sqlInsert3);
                         System.out.println("Filas afectadas: " + rowsAffected3);
                         break;
                     case 4:
                         System.out.print("Introduce DNI: ");
                         dni=sc.nextLine();
-                        String sqlInsert4 = "DELETE alumnos, matricula FROM alumnos JOIN matricula ON alumnos.dni = matricula.dni WHERE alumnos.dni = '"+dni+"';";
+                        String sqlInsert4 = "DELETE FROM matricula WHERE dniAlumno = '"+dni+"';";
+                        Statement stmt4 = conexion.createStatement();
+                        int rowsAffected4 = stmt4.executeUpdate(sqlInsert4);
+
+                        String sqlInsert5 = "DELETE FROM alumno WHERE dni = '"+dni+"';";
+                        Statement stmt5 = conexion.createStatement();
+                        int rowsAffected5 = stmt4.executeUpdate(sqlInsert5);
+
+                        System.out.println("Filas afectadas: " + rowsAffected4);
+                        System.out.println("Filas afectadas: " + rowsAffected5);
                         break;
                     default:
                         throw new AssertionError("Opción no válida.");

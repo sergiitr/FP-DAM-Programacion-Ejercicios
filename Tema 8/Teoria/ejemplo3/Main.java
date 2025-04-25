bd49ded84face67bb3fa0271f925415eb5501257
Sergio Trillo Rodriguez <86511887+sergiitr@users.noreply.github.com>
2025-04-25 17:28:14 +0200

    Update Main.java

--- Tema 8/Teoria/ejemplo3/Main.java
+++ Tema 8/Teoria/ejemplo3/Main.java
@@ -6,25 +6,14 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            Scanner sc = new Scanner(System.in);
+    public static void funcion(double notaCorte,String asignatura) {
+        try{
             final String USUARIO = "usuario";
             final String CONTRASENIA = "usuario";
             Connection con;
             String url = "jdbc:mysql://localhost/academia";
             con = DriverManager.getConnection(url, USUARIO, CONTRASENIA);
             System.out.println("Conexión exitosa.");
-            
-            
-            double notaCorte;
-            String asignatura;
-
-            System.out.print("Introduce nota de corte: ");
-            notaCorte=sc.nextDouble();
-            sc.nextLine();
-            System.out.print("Introduce asignatura: ");
-            asignatura=sc.nextLine();
 
             String sql = "SELECT dni,alumno.nombre FROM alumno,asignatura,matricula WHERE matricula.nota >= ? AND dni=dniAlumno AND asignatura.idAsignatura=matricula.idAsignatura AND asignatura.nombre=?;";
             PreparedStatement sentencia = con.prepareStatement(sql);
@@ -36,13 +25,34 @@ public class Main {
             System.out.println("DNI\t\tNombre");
             while(rs.next()) {
                 System.out.println(rs.getString("dni")+"\t"+rs.getString("nombre"));   
-            }   
-                
+            }
+
             con.close();
         } catch (SQLException ex) {
             System.out.println("Error al conectar a la base de datos.");
             ex.printStackTrace();
         }
+    }
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            double notaCorte;
+            String asignatura;
+            int salida;
+            do {
+                System.out.print("Introduce nota de corte: ");
+                notaCorte=sc.nextDouble();
+                sc.nextLine();
+                System.out.print("Introduce asignatura: ");
+                asignatura=sc.nextLine();
+    
+                funcion(notaCorte,asignatura);
+                do { 
+                    System.out.print("Deseas salir? (0 no 1 si): ");
+                    salida=sc.nextInt();
+                } while (salida!=0 && salida != 1);
+                System.out.println();
+            }while (salida!=1);
+
     }
 }
 

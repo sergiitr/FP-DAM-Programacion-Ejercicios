286968af897325873b7e87bea4fe1bd262789050
Sergio Trillo Rodriguez <86511887+sergiitr@users.noreply.github.com>
2025-04-30 16:18:32 +0200

    Update Main.java

--- Tema 8/Ejercicios/ejercicio5/Main.java
+++ Tema 8/Ejercicios/ejercicio5/Main.java
@@ -1,3 +1,6 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.Scanner;
 
@@ -5,24 +8,96 @@ public class Main {
     public static Scanner sc = new Scanner (System.in);
     
     public static void anadirAlumno() {
-        LocalDate fechaNacimiento;
-        String dni,nombre,apellidos,direccion,email;
-        fechaNacimiento.now();
-        System.out.print("Introduce DNI:");
-        dni=sc.nextLine();
-        System.out.print("Introduce nombre:");
-        nombre=sc.nextLine();
-        System.out.print("Introduce apellidos:");
-        apellidos=sc.nextLine();
-        System.out.print("Introduce direccion:");
-        direccion=sc.nextLine();
-        System.out.print("Introduce email:");
-        email=sc.nextLine();
+        try {
+            Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/academia", "usuario", "usuario");
+            AlumnoDAO alumnos = new AlumnoDAO(conexion);
 
-        Alumno alumno = new Alumno(dni, nombre, apellidos, fechaNacimiento, direccion, email);
-        AlumnoDAO alumnos;
-        alumnos.crearAlumno(alumno);
+            LocalDate fechaNacimiento = LocalDate.now();
+            String dni, nombre, apellidos, direccion, email;
+
+            System.out.print("Introduce DNI:");
+            dni = sc.nextLine();
+            System.out.print("Introduce nombre:");
+            nombre = sc.nextLine();
+            System.out.print("Introduce apellidos:");
+            apellidos = sc.nextLine();
+            System.out.print("Introduce direccion:");
+            direccion = sc.nextLine();
+            System.out.print("Introduce email:");
+            email = sc.nextLine();
+
+            if ( alumnos.obtenerAlumnoPorDni(dni) == null ){
+                Alumno alumno = new Alumno(dni, nombre, apellidos, fechaNacimiento, direccion, email);
+                alumnos.crearAlumno(alumno);
+            } else
+                System.out.println("Alumno ya está en el sistema");  
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
+    public static void mostrarDatosAlumno() {
+        try {
+            String dni;
+            Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/academia", "usuario", "usuario");
+            AlumnoDAO alumnos = new AlumnoDAO(conexion);
+            
+            System.out.print("Introduce DNI del alumno: ");
+            dni=sc.nextLine();
+
+            System.out.println(alumnos.obtenerAlumnoPorDni(dni).toString());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void actualizarDatos() {
+        try {
+            String dni;
+            Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/academia", "usuario", "usuario");
+            AlumnoDAO alumnos = new AlumnoDAO(conexion);
+
+            System.out.print("Introduce DNI del alumno: ");
+            dni=sc.nextLine();
+
+            Alumno alumno = alumnos.obtenerAlumnoPorDni(dni);
+
+            alumnos.actualizarAlumno(alumno);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void eliminarAlumno() {
+        try {
+            String dni;
+            Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/academia", "usuario", "usuario");
+            AlumnoDAO alumnos = new AlumnoDAO(conexion);
+
+            System.out.print("Introduce DNI del alumno: ");
+            dni=sc.nextLine();
+            alumnos.eliminarAlumno(dni);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void mostrarDatos() {
+        try {
+            String dni;
+            Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/academia", "usuario", "usuario");
+            AlumnoDAO alumnos = new AlumnoDAO(conexion);
+
+            
+            for (int i = 0; i < alumnos.obtenerTodosalumno().size(); i++) {
+                System.out.println(alumnos.obtenerTodosalumno().get(i).toString());
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int opcion;
@@ -36,16 +111,28 @@ public class Main {
             System.out.println("6) Salir");
             
             do { 
-                System.out.print("\tIntroduce opcion: ");
+                System.out.print("Introduce opcion: ");
                 opcion=sc.nextInt();
-            } while (opcion>=1 && opcion<=6);
+            } while (opcion<1 && opcion>6);
 
             switch (opcion) {
                 case 1:
                     anadirAlumno();
                     break;
+                case 2:
+                    mostrarDatosAlumno();
+                    break;
+                case 3:
+                    actualizarDatos();
+                    break;
+                case 4:
+                    eliminarAlumno();
+                    break;
+                case 5:
+                    mostrarDatos();
+                    break;
                 default:
-                    throw new AssertionError();
+                    break;
             }
         } while (opcion!=6);
     }

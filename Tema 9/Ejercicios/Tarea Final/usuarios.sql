af08b12902eae0e436842f015749322af57d5f63
Sergio Trillo Rodriguez <86511887+sergiitr@users.noreply.github.com>
2025-05-19 16:57:06 +0200

    Delete Tema 9/Ejercicios/Tarea Final directory

--- Tema 9/Ejercicios/Tarea Final/usuarios.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS "usuarios"
-(
-    [id] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    [nombre] NVARCHAR(160)  NOT NULL,
-    [numero] INTEGER(9)  NOT NULL,
-    [correo] NVARCHAR(160)  NOT NULL
-);
-INSERT INTO usuarios (nombre, numero, correo) VALUES ('aaa','111','aaa');
